36fc53c06fc82ad1e02cb7d8b8fb947688e0c2b2
Adrian Norberto Marino <adrianmarino@gmail.com>
2013-05-18 16:39:55 -0300

    Training test data ok

--- generate_training_data.rb
+++ generate_training_data.rb
@@ -2,7 +2,12 @@ require './helper'
 
 puts "Remove old file..."
 delete_dao_file
+
 tests_count = ARGV[0].to_i
 puts "Generate #{tests_count} random tests..."
 tests = MMPI2Generator.instance.generate tests_count
+
+puts "Calculate results for all tests..."
+tests.each {|a_test|a_test.calculate_results}
+
 MMPI2Dao.new(MMPI2_DAO_FILE_NAME).save tests
