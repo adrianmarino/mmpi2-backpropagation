3242338e91dfcd60111d95ad2d2ebdb759cd0708
Adrian Norberto Marino <adrianmarino@gmail.com>
2013-05-19 15:36:46 -0300

    Config comments.

--- config/net_configuration.rb
+++ config/net_configuration.rb
@@ -3,9 +3,18 @@ require "../lib/binary_output_converter"
 require "../lib/output_by_number_converter"
 
 module NetConfiguration
-	NEURON_LEVELS = [32,20,5]
+	# Neurons by level...
+	NEURON_LEVELS = [32,50,50,5]
+
+	# Learning rate: beetween 0,05 and 0,25...
 	LEARNING_RATE = 0.25
+
+	# Convergence speed...
 	MOMENTUM = 0.1
+
+	# Max net error...
 	MAX_ERROR = 0.001
+
+	# Output converter used...
 	OUTPUT_CONVERTER = BinaryOutputConverter.new 5
 end
