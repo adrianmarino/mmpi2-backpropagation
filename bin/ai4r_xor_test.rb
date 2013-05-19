9ce289bfcba5ffddfae079b90ee39e8b2a8b8c83
Adrian Norberto Marino <adrianmarino@gmail.com>
2013-05-19 12:03:28 -0300

    Improve output

--- bin/ai4r_xor_test.rb
+++ bin/ai4r_xor_test.rb
@@ -1,18 +1,19 @@
-require "./lib/helper"
+require "./helper"
 
-# Create the network with:
+# Create "Input: #{j} , he network with:
 #   2 inputs
 #   1 hidden layer with 3 neurons
 #   1 outputs
 net = Ai4r::NeuralNetwork::Backpropagation.new([2, 3, 1])
-
+#  Input =>  1     2     3     4       
 example = [[0,0],[0,1],[1,0],[1,1]]
 result = [[0],[1],[1],[0]]
 
 # Train the network
-400.times do |i|
-  j = i % result.length
-  puts net.train(example[j], result[j])
+400.times do |an_index|
+  position = an_index % result.length
+  error = net.train(example[position], result[position])
+  puts "Train time #{an_index+1},	Input: #{position+1} => Error: #{error}" 
 end
 
 # Use it: Evaluate data with the trained network
