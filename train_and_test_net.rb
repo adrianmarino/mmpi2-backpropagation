10734bf3e1f8bd2fa881a824b13f77b4c44e323e
Adrian Norberto Marino <adrianmarino@gmail.com>
2013-05-19 10:41:04 -0300

    train and test net

---
+++ train_and_test_net.rb
@@ -0,0 +1,19 @@
+require './helper'
+
+# Parameneters...
+tests_count = ARGV[0].to_i
+
+puts "Generate #{tests_count} random tests..."
+tests = MMPI2Generator.instance.generate tests_count
+
+net = MMPI2BackPropagation.new_configured
+puts "Training net..."
+net.train(tests)
+
+test = MMPI2Generator.instance.generate.first
+puts "Test Depression Level: #{test.depression_level}" 
+
+puts "Eval test on net..."
+result = net.results_of test
+
+puts result
