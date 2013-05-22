8cdd9332a8030bf262f3353ca700688beb762fa0
Adrian Norberto Marino <adrianmarino@gmail.com>
2013-05-22 09:03:53 -0300

    Add user tests interface

---
+++ bin/mmp12_test.rb
@@ -0,0 +1,24 @@
+require './helper'
+
+puts "--------------"
+puts "| Test MMPI2 |"
+puts "--------------\n\n"
+puts "Responda las siguientes afirmaciones:\n\n"
+
+test = MMPI2Factory.instance.new_test
+
+count = 1
+test.sentences.each { |a_sentence|
+	puts "#{count}. #{a_sentence.text}. Responda: s/n"
+	response = gets.chomp
+	a_sentence.answer_yes if response == 's'
+	a_sentence.answer_no if response == 'n'
+	count+=1
+}
+
+net = ObjectDao.new(NET_FILE_NAME).load
+
+puts "Eval test on net..."
+result = net.results_of test
+
+puts result
