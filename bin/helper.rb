8a979a190f11ab78e109a8cdc003852e07cd4737
Adrian Norberto Marino <adrianmarino@gmail.com>
2013-05-25 11:16:10 -0300

    simplify helper file

--- bin/helper.rb
+++ bin/helper.rb
@@ -1,29 +1,3 @@
-# ----------------------------------------------------------------------------
-# Require
-# ----------------------------------------------------------------------------
-require 'rubygems'
-require 'bundler/setup'
-require "require_all"
-
-require "yaml"
-require "singleton"
-require "ai4r"
-
-require_all '../lib'
-require_all '../config'
-
-# ----------------------------------------------------------------------------
-# Constants
-# ----------------------------------------------------------------------------
-SENTENCES_FILE_NAME = '../config/sentences'
-NET_FILE_NAME = 'net_trained.dat'
-LOGGER_PATH="../log"
-LOGGER_FILE_NAME="#{LOGGER_PATH}/mmpi2.log"
-READ_ONLY='r'
-WRITE_ONLY='w'
-YES='Si'
-NO='No'
-
-
-
+ROOT_DIR=".."
+require "#{ROOT_DIR}/lib/generals"
 
--- test/helper.rb
+++ test/helper.rb
@@ -1,22 +1,2 @@
-require 'rubygems'
-require 'bundler/setup'
-require "require_all"
-
-require "yaml"
-require "singleton"
-require "ai4r"
-
-require_all 'lib'
-require_all 'config'
-
-# ----------------------------------------------------------------------------
-# Constants
-# ----------------------------------------------------------------------------
-SENTENCES_FILE_NAME = './config/sentences'
-NET_FILE_NAME = 'net_trained.dat'
-READ_ONLY='r'
-WRITE_ONLY='w'
-YES='Si'
-NO='No'
-LOGGER_PATH="./log"
-LOGGER_FILE_NAME="#{LOGGER_PATH}/mmpi2.log"
+ROOT_DIR="."
+require "#{ROOT_DIR}/lib/generals"
